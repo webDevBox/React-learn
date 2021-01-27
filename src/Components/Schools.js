152a4ed0e9dbaee67b2bb2b7a9bfcb67b47be2ec
Aoun420 <mmaoun344@gmail.com>
2021-01-27 10:46:11 +0500

    a

--- src/Components/Schools.js
+++ src/Components/Schools.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Myheader from './Myheader'
 import './styles.css'
+import MyMenu from './MyMenu'
 
 function Schools(){
 
     return(
-        <>
+        <> 
         <Myheader/>
         <div className='row'>
-            <MyMenu
-            <div className='col-md-offset-3'>
+
+            <MyMenu/>
+            <div className='col-md-offset-2 col-lg-offset-2 col-lg-5 col-md-5 col-sm-12 col-xs-12 '>
         <h1 className='green'>This is a Schools Component</h1>
         </div>
         </div>
